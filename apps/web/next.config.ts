5fdc7fe9ac2b14ccbd03fb626686d0e85be2ce54
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-02 16:23:29 +0600

    chore: updated config and added  missing packages

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+    remotePatterns: [
+      { hostname: `images.unsplash.com` },
+      { hostname: `raw.githubusercontent.com` },
+    ],
+  },
+};
 
 export default nextConfig;
