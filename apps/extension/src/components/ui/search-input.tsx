e9dd806150460dfd6bc31e74ba5724a97ec2ce3f
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-10 17:07:48 +0600

    feat: created search and showed initial prompts

---
+++ apps/extension/src/components/ui/search-input.tsx
@@ -0,0 +1,29 @@
+import { Search as SearchIcon } from "lucide-react";
+
+interface Props extends React.ComponentProps<"input"> {}
+
+export const SearchInput = ({ ...props }: Props) => {
+  return (
+    <div>
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <SearchIcon className="size-5 text-gray-500" />
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search prompts..."
+          required
+          {...props}
+        />
+      </div>
+    </div>
+  );
+};
