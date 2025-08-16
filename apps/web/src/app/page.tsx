ffdf4017cae24296f68e41a730f070e115447fb6
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-16 15:13:11 +0600

    fix: web layout and added assets packages

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,16 @@
-import { BentoGrid } from "@workspace/ui/kokonutui/bento-grid";
 import { FeaturesSection } from "@/components/feature/homepage/features-section";
 import { FooterSection } from "@/components/feature/homepage/footer-section";
 import { HeroSection } from "@/components/feature/homepage/hero";
 import { TrendingPromptsSection } from "@/components/feature/homepage/trending-prompts-section";
 import { NavigationBar } from "@/components/shared/navigation-bar";
+import BentoDemo from "./bento-demo";
 
 const RootPage = () => {
   return (
     <div className="flex w-full flex-col items-center overflow-x-hidden bg-black px-4">
       <NavigationBar />
       <HeroSection />
-      <BentoGrid />
+      <BentoDemo />
       <TrendingPromptsSection />
       <FeaturesSection />
       <FooterSection />
