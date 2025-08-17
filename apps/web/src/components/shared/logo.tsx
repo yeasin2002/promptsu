16c3b42ad76d01389ce29fcdd4a7039cecc9fe2d
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-17 21:08:24 +0600

    chore: configuration assets packages

--- apps/web/src/components/shared/logo.tsx
+++ apps/web/src/components/shared/logo.tsx
@@ -1,14 +1,15 @@
 interface Props extends React.ComponentProps<'div'> {}
 
+import logo from '@workspace/assets/images/logo.svg';
+import Image from 'next/image';
+
 export const Logo = ({ ...props }: Props) => {
   return (
     <div
       className="flex items-center justify-center gap-2 lg:gap-[15px]"
       {...props}
     >
-      <div className="flex h-6 w-6 items-center justify-center rounded-lg bg-gradient-to-br from-purple-400 to-blue-500 lg:h-[30.75px] lg:w-[30.75px]">
-        <span className="font-bold text-white text-xs lg:text-sm">P</span>
-      </div>
+      <Image alt="Logo" className="size-8" src={logo} />
       <div className="relative">
         <span className="text-center font-manrope font-medium text-lg text-white leading-[normal] tracking-[0] lg:text-[22px]">
           Promptverse
