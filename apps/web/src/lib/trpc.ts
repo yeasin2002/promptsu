8fdc4a30c23c355e4a732404561148e4099f2772
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-08-27 22:16:04 +0600

    fix: tRPC server route issues

--- apps/web/src/lib/trpc.ts
+++ apps/web/src/lib/trpc.ts
@@ -1,8 +1,5 @@
 import { QueryCache, QueryClient } from '@tanstack/react-query';
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
-import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import { toast } from 'sonner';
-import type { trpcAppRouter } from "../../../server/src/routers";
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -19,21 +16,21 @@ export const queryClient = new QueryClient({
   }),
 });
 
-const trpcClient = createTRPCClient<trpcAppRouter>({
-  links: [
-    httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/trpc`,
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: "include",
-        });
-      },
-    }),
-  ],
-});
+// const trpcClient = createTRPCClient<trpcAppRouter>({
+//   links: [
+//     httpBatchLink({
+//       url: `${process.env.NEXT_PUBLIC_SERVER_URL}/trpc`,
+//       fetch(url, options) {
+//         return fetch(url, {
+//           ...options,
+//           credentials: "include",
+//         });
+//       },
+//     }),
+//   ],
+// });
 
-export const trpc = createTRPCOptionsProxy<trpcAppRouter>({
-  client: trpcClient,
-  queryClient,
-});
+// export const trpc = createTRPCOptionsProxy<trpcAppRouter>({
+//   client: trpcClient,
+//   queryClient,
+// });
