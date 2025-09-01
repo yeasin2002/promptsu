232938b5d5f4560a2491768fb2a5ba7fdaaa345a
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-09-01 11:34:39 +0600

    refactor: removed native app - [temporary]

--- apps/native/components/container.tsx
+++
@@ -1,8 +0,0 @@
-import type React from 'react';
-import { SafeAreaView } from 'react-native';
-
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <SafeAreaView className="flex-1 bg-background">{children}</SafeAreaView>
-  );
-};
--- apps/native/components/tabbar-icon.tsx
+++
@@ -1,8 +0,0 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-
-export const TabBarIcon = (props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) => {
-  return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
-};
