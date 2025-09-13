2261cb4ccb4f8fbd609433fba7c7011a063a921c
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-09-13 17:12:03 +0600

    chore: configured oRPC

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
 import { BottomCta, FeaturesSection, Hero, TestimonialsSection } from '@/components/feature/homepage';
 import { FlowingWaveRays } from '@/components/feature/homepage/flowing-wave-rays';
 import { PricingSection } from '@/components/feature/pricing';
 import { Footer, Navigation } from '@/components/shared';
+import { orpc } from '@/utils/orpc';
 
 const RootPage = () => {
+  const healthCheck = useQuery(orpc.healthCheck.queryOptions());
+  console.log('🚀 ~ RootPage ~ healthCheck:', healthCheck.data);
   return (
     <div className=" absolute inset-0 bg-black">
       <FlowingWaveRays />
-
       <div className="container mx-auto min-h-screen text-foreground">
         <Navigation />
         <Hero />
