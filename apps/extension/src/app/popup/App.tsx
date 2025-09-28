da6dc750f8e240e0077cbe2c511c595b27c72119
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-09-28 14:12:43 +0600

    chore(extension): configured shadcn-ui for Extension

--- apps/extension/src/app/popup/App.tsx
+++ apps/extension/src/app/popup/App.tsx
@@ -1,60 +1,55 @@
-import { Button } from "@workspace/ui/shadcn/button";
+import { Button } from "@/components/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@workspace/ui/shadcn/card";
-import { Input } from "@workspace/ui/shadcn/input";
-import { Label } from "@workspace/ui/shadcn/label";
-import {
-	Tabs,
-	TabsContent,
-	TabsList,
-	TabsTrigger,
-} from "@workspace/ui/shadcn/tabs";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CTA1 } from "@workspace/ui/workspace-ui/CTA";
 
 function App() {
-	return (
-		<div className="flex w-full max-w-sm flex-col gap-6 min-w-xl p-4">
-			<Tabs defaultValue="account">
-				<TabsList>
-					<TabsTrigger value="account">Account</TabsTrigger>
-					<TabsTrigger value="password">Password</TabsTrigger>
-				</TabsList>
-				<TabsContent value="account">
-					<CTA1 />
-				</TabsContent>
-				<TabsContent value="password">
-					<Card>
-						<CardHeader>
-							<CardTitle>Password</CardTitle>
-							<CardDescription>
-								Change your password here. After saving, you&apos;ll be logged
-								out.
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="grid gap-6">
-							<div className="grid gap-3">
-								<Label htmlFor="tabs-demo-current">Current password</Label>
-								<Input id="tabs-demo-current" type="password" />
-							</div>
-							<div className="grid gap-3">
-								<Label htmlFor="tabs-demo-new">New password</Label>
-								<Input id="tabs-demo-new" type="password" />
-							</div>
-						</CardContent>
-						<CardFooter>
-							<Button>Save password</Button>
-						</CardFooter>
-					</Card>
-				</TabsContent>
-			</Tabs>
-		</div>
-	);
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 min-w-xl p-4">
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <CTA1 />
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you&apos;ll be logged
+                out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-current">Current password</Label>
+                <Input id="tabs-demo-current" type="password" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-new">New password</Label>
+                <Input id="tabs-demo-new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default App;
