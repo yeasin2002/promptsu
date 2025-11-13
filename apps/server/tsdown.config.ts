f7a38e3822c70b9472a428831bd7aa38ad5cd051
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-13 21:59:15 +0600

    chore: updated config for edge build

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  bundle: true,
+  outDir: 'dist',
+  format: 'esm',
 });
+
+
 
 // external: ["@neondatabase/serverless", "better-auth", "drizzle-orm"],
