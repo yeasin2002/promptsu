ac18508781ef0f34ac24247d2ac2ec64c3f1e929
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-14 15:31:23 +0600

    feat: added AI API handlers

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,14 +1,17 @@
 import cors from "cors";
 import express from "express";
+import mainRouter from "./routes";
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", async (req, res) => {
+app.get("/", (_req, res) => {
   res.json({ message: "Hello World" });
 });
+
+app.use("/api", mainRouter);
 
 // Start the server
 app.listen(PORT, () =>
